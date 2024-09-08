0943b8315d81173025881ac6b3513d6dfcc4b41b
Daan Lenaerts <me@daanlenaerts.com>
2024-09-08 14:39:07 +0200

    Fix Drizzle config

--- paperjet-app/drizzle.config.ts
+++ paperjet-app/drizzle.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/db/schema.ts",
-  out: "./src/db/migrations",
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DB_CONNECTION_STRING!,
